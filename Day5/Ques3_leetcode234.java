09afae60be16e0926e39c3eda7cf8831428e7b92
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-04-28 13:29:30 +0530

    Leetcode Ques 234

---
+++ Day5/Ques3_leetcode234.java
@@ -0,0 +1,33 @@
+public class Ques3_leetcode234 {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+        secondHalf = reverseList(secondHalf);
+
+        ListNode firstHalf = head;
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
