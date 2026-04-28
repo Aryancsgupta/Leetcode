3d828867e6512f1e7bfffbdf7ebe3933101d5f65
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-04-28 13:25:56 +0530

    Leetcode Ques 143

---
+++ Day5/Ques1_leetcode143.java
@@ -0,0 +1,39 @@
+class Ques1_leetcode143 {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+    
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+        secondHalf = reverseList(secondHalf);
+
+       
+        ListNode firstHalf = head;
+        while(secondHalf != null){
+            ListNode temp1 = firstHalf.next;
+            ListNode temp2 = secondHalf.next;
+
+            firstHalf.next = secondHalf;
+            secondHalf.next = temp1;
+
+            firstHalf = temp1;
+            secondHalf = temp2;
+        }
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
